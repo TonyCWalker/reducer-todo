03069a03cd82873438896ef10cb0945ad8e60aa7
Tony Walker <Walkertony509@gmail.com>
2020-02-04 12:26:24 -0800

    I have created the base app structure with create react app and then added a reducer function.

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    item: "",
+    completed: false,
+    id: ""
+  };
+  
+  export const reducer = (state, action) => {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  };
